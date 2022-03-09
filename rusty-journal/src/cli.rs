e21966a3944e50cc700fb002feba15988b4a3311
takuyaw-w <takuya.jamtb@gmail.com>
2022-03-10 00:58:06 +0900

    feat: 定義

---
+++ rusty-journal/src/cli.rs
@@ -0,0 +1,27 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    Add {
+        #[structopt()]
+        text: String,
+    },
+    Done {
+        #[structopt()]
+        position: usize,
+    },
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Rusty Journal",
+    about = "A command line to-do app written in Rust"
+)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
